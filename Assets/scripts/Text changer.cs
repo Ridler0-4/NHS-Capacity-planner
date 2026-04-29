1cbe3565e9c10cd12e7a7831d63b106e51fa5e10
Ridler0-4 <bi77sg@student.sunderland.ac.uk>
2026-04-30 00:47:53 +0100

    Added script to deit text based on current capacity level

--- Assets/scripts/Text changer.cs
+++ Assets/scripts/Text changer.cs
@@ -5,18 +5,30 @@ using UnityEngine.UI;
 public class Textchanger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    int capacityLevel = 100;
+    double capacityLevel = 100;
     [SerializeField]
-    Text Status;
+    TextMeshProUGUI Status;
 
     void Start()
     {
-        
+        Status.text = "Standard";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        capacityLevel = capacityLevel - 0.01;
+        if (capacityLevel <= 90)
+        {
+            Status.text = "Intervention necessary, capacity under 90%";
+        }
+        else if (capacityLevel <= 95)
+        {
+            Status.text = "Warning, Capacity under 95%";
+        }
+        else
+        {
+            Status.text = "Normal";
+        }
     }
 }
