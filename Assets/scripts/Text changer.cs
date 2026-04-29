8a6020ff56ae3469802bd5442480b988f814e2f1
Ridler0-4 <bi77sg@student.sunderland.ac.uk>
2026-04-30 00:31:12 +0100

    Added NHS logo

---
+++ Assets/scripts/Text changer.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Textchanger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int capacityLevel = 100;
+    [SerializeField]
+    Text Status;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
